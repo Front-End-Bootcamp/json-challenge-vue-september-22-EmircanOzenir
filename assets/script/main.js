846d717b58be749d21a950fd7a30920388e26e88
Emircan Özenir <81494873+EmircanOzenir@users.noreply.github.com>
2022-09-15 14:36:00 +0300

    Created the json data and added its source into index.html.
    Created a function which is called filterByGroupName in main.js.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,30 @@
-console.log('👻');
+// console.log('👻');
+
+const filterByGroupName = (property) => {
+    
+    // Created a null object, thus I'll be able to push some values into it.
+    const resultObj = {
+        "group": null,
+        "student": [],
+        "assistant": null
+    };
+    
+    //fetched the data, and checked if the group name matched with our parameter (which is called property).
+
+    fetch('/assets/data/data.json')
+    .then(response => response.json())
+    .then(data => data.filter(person => person.group === property)
+
+    // then iterated in the results whose are matched with the property
+    .forEach(person => {
+        resultObj.group = property
+        person.type ? resultObj.assistant = person.name : resultObj.student.push(person.name)
+    }))
+
+    // catched if there is an error
+    .catch(error => alert(error));
+
+    return resultObj;
+}
+
+console.log(filterByGroupName("YellowGreen"));
